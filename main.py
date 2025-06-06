401fcf91e82e853d8f7696aaea76649282732eb3
terrdapear <t3rrenceissocool@gmail.com>
2025-06-07 01:21:09 +0800

    WORKING HALF OF THE TUTORIAL

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
 import pygame
 import assets.objects as objects
 
-
+temp=[]
 #important shit
 pygame.init()
+pygame.mixer.init()
 screen = pygame.display.set_mode((1280,720))
 clock = pygame.time.Clock()
 pygame.display.set_caption("TITLE")
 
+for i in range(5):
+    objects.tutorial["frames"][i] = objects.tutorial["frames"][i].convert_alpha()
+
 colors = {"z":"blue","x":"red","cz":"purple","cx":"orange"}
 
 #==========================global stuff
-state=["menu"]
+sounds = {"hit":pygame.mixer.Sound(objects.sounds["hit"])}
+state=["titlescreen"]
 settings={
     "beat_speed":10
 }
@@ -33,27 +38,11 @@ def update_beat():
             objects.beats.pop(0)
     
 #========== WINDOWS ===========#
-def menu():
+def titlescreen():
     pass
 
-def level1():
-    #bacgkround
-    bg = pygame.Surface((1280,720))
-    bg.fill("Black")
-
-    screen.blit(bg,(0,0))
-    #background
-    #track
-    screen.blit(objects.bamboo.convert_alpha(),(0,17))
-    screen.blit(receiver[0].surface,receiver[0].rect)
-
-    #track
-
-    if len(track)>0 and pygame.time.get_ticks()-start[0]+delay[0]>track[0][0]:
-        objects.beats.append(objects.beat(place="up",color=colors[track[0][1]]))
-        track.pop(0)
-    update_beat()
-
+def scene1():
+    pass
     
 def tutorial():
     #bacgkround
@@ -66,11 +55,23 @@ def tutorial():
     screen.blit(objects.bamboo.convert_alpha(),(0,17))
     screen.blit(receiver[0].surface,receiver[0].rect)
     #track
+    tim=pygame.time.get_ticks()-start[0]+delay[0]
 
-    if len(track)>0 and pygame.time.get_ticks()-start[0]+delay[0]>track[0][0]:
+    if len(track)>0 and tim>track[0][0]:
         objects.beats.append(objects.beat(place="up",color=colors[track[0][1]]))
         track.pop(0)
+    if len(objects.tutorial["track"])>1 and tim-delay[0]<objects.tutorial["track"][1][0]:
+        screen.blit(objects.tutorial["frames"][objects.tutorial["track"][0][1]-1],(0,136))
+    elif len(objects.tutorial["track"])>1:
+        objects.tutorial["track"].pop(0)
+    screen.blit(objects.tutorial["frames"][objects.tutorial["track"][0][1]-1],(0,136))    
+    
     update_beat()
+    if tim>106_000:
+        state[0]="scene2"
+
+def scene2():
+    pass
 
 def settings_menu():
     pass
@@ -83,6 +84,7 @@ def explode():
 def success():
     combo[0]+=1
     objects.beats.pop(0)
+    sounds["hit"].play()
     pass
 
 def fail():
@@ -104,7 +106,7 @@ def lodi():
 #============= EFFECTS ============#
 
 def main():
-    states = {"menu":menu,"tutorial":tutorial,"settings_menu":settings_menu,"level1":level1}
+    states = {"titlescreen":titlescreen,"tutorial":tutorial,"settings_menu":settings_menu,"scene2":scene2,"scene1":scene1}
     while True:
         for event in pygame.event.get():
 
@@ -114,11 +116,11 @@ def main():
             if event.type == pygame.KEYDOWN:
                 if event.key==32: #space
                     state[0]="tutorial"
-                    start[0]=pygame.time.get_ticks()
+                    start[0]=pygame.time.get_ticks()-100
                     for i in objects.songs["tutorial"]['track']:
                         track.append(i)
                     pygame.mixer.music.load(objects.songs["tutorial"]["path"])
-                    pygame.mixer.music.play()
+                    pygame.mixer.music.play()   
 
                 if event.key==122:
                     if len(objects.beats)>0:
@@ -153,6 +155,8 @@ def main():
                         else:
                             fail()
                     
+                if event.key==112:
+                    temp.append(pygame.time.get_ticks()-start[0])
                 #print("key:",event.key)
             
             if event.type == pygame.KEYUP:
@@ -173,4 +177,5 @@ def main():
         
 
 main()
+#print(temp)
 pygame.quit()
